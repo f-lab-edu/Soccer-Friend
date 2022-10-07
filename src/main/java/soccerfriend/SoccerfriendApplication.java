d317b693df3e953dc96cf4d2e0543e99e47d7c23
xeonu <sunwoo1029@naver.com>
2022-10-07 22:17:48 +0900

    프로젝트 생성

---
+++ src/main/java/soccerfriend/SoccerfriendApplication.java
@@ -0,0 +1,13 @@
+package soccerfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerfriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerfriendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/soccerfriend/SoccerfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package soccerfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerfriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
