604d3d414b763e9895123d06e4e9c24a362613a6
xeonu <sunwoo1029@naver.com>
2022-10-16 21:19:23 +0900

    service 어노테이션 추가

--- src/main/java/soccerfriend/service/SessionAuthorizeService.java
+++ src/main/java/soccerfriend/service/SessionAuthorizeService.java
@@ -1,12 +1,14 @@
 package soccerfriend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
 import static soccerfriend.service.LoginService.LOGIN_MEMBER;
 
 @RequiredArgsConstructor
+@Service
 public class SessionAuthorizeService implements AuthorizeService {
     private HttpSession httpSession;
 
