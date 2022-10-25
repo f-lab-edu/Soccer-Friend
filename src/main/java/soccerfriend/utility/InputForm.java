f83d7f7f8207abba7baf4df5f071d506ee3fa458
xeonu <sunwoo1029@naver.com>
2022-10-25 10:45:33 +0900

    Request @Setter 삭제 후 @AllArgsConstructor 추가

--- src/main/java/soccerfriend/utility/InputForm.java
+++ src/main/java/soccerfriend/utility/InputForm.java
@@ -1,14 +1,14 @@
 package soccerfriend.utility;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 public class InputForm {
     /**
      * password를 변경하기 위해 입력해야 하는 값들
      */
     @Getter
-    @Setter
+    @AllArgsConstructor
     public static class UpdatePasswordRequest {
         private String before;
         private String after;
@@ -18,7 +18,7 @@ public class InputForm {
      * login 하기위해 입력해야 하는 값들
      */
     @Getter
-    @Setter
+    @AllArgsConstructor
     public static class LoginRequest {
         String id;
         String password;
@@ -28,7 +28,7 @@ public class InputForm {
      * stadiumOwner의 정보를 변경하기위해 입력해야 하는 값들
      */
     @Getter
-    @Setter
+    @AllArgsConstructor
     public static class UpdateStadiumOwnerRequest {
         String representative;
         String companyName;
