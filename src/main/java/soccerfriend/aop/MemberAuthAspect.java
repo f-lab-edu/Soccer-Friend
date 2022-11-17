a95bda63ecc757cc2034a191753cc6451fbccb07
xeonu <sunwoo1029@naver.com>
2022-11-17 20:45:05 +0900

    annotation pointcut에 패키지 이름이 controller에서만 적용되게 수정

--- src/main/java/soccerfriend/aop/MemberAuthAspect.java
+++ src/main/java/soccerfriend/aop/MemberAuthAspect.java
@@ -4,12 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-import soccerfriend.exception.exception.NoPermissionException;
 import soccerfriend.service.LoginService;
 import soccerfriend.service.MemberService;
-
-import static soccerfriend.exception.ExceptionInfo.CHANGE_PASSWORD_REQUIRED;
-import static soccerfriend.utility.PasswordWarning.NO_WARNING;
 
 @RequiredArgsConstructor
 @Aspect
@@ -20,13 +16,9 @@ public class MemberAuthAspect {
 
     /**
      * member의 로그인 여부를 확인합니다.
-     * 로그인한 member의 비밀번호 상태에 이상이 있는지 확인합니다.
      */
-    @Before("@annotation(soccerfriend.aop.MemberLoginCheck)")
+    @Before("@annotation(soccerfriend.aop.MemberLoginCheck) && execution(* soccerfriend.controller..*.*(..))")
     public void memberAuthentication() {
         int memberId = loginService.getMemberId();
-        if (memberService.getPasswordWarning(memberId) != NO_WARNING.getCode()) {
-            throw new NoPermissionException(CHANGE_PASSWORD_REQUIRED);
-        }
     }
 }
