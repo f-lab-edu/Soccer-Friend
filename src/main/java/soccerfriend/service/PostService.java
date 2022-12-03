162a4294a4b1c9e53ca1f9677cc48337e08197b5
xeonu <sunwoo1029@naver.com>
2022-12-03 18:32:52 +0900

    post 읽은 기록정보를 쿠키에서 redis 캐시로 변경

--- src/main/java/soccerfriend/service/PostService.java
+++ src/main/java/soccerfriend/service/PostService.java
@@ -11,9 +11,6 @@ import soccerfriend.exception.exception.BadRequestException;
 import soccerfriend.exception.exception.NoPermissionException;
 import soccerfriend.mapper.PostMapper;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
 import java.util.List;
 
@@ -131,11 +128,9 @@ public class PostService {
      *
      * @param memberId 게시물을 조회하는 member의 id
      * @param id       게시물의 id
-     * @param req
-     * @param res
      * @return 특정 id의 게시물
      */
-    public Post readPost(int memberId, int id, HttpServletRequest req, HttpServletResponse res) {
+    public Post readPost(int memberId, int id) {
         Member member = memberService.getMemberById(memberId);
         if (member == null) {
             throw new BadRequestException(MEMBER_NOT_EXIST);
@@ -145,9 +140,11 @@ public class PostService {
             throw new BadRequestException(POST_NOT_EXIST);
         }
 
-        if (!postViewCheck(req, res, id)) {
+        if (!postViewCheck(memberId, id)) {
             increaseViews(id);
         }
+
+        redisTemplate.opsForValue().set("POST " + id + " MEMBER " + memberId, 1, Duration.ofDays(1));
 
         return post;
     }
@@ -155,42 +152,15 @@ public class PostService {
     /**
      * 해당 게시물을 24시간 이내에 읽었는지 확인합니다.
      *
-     * @param req
-     * @param res
-     * @param id  게시물의 id
+     * @param id 게시물의 id
      * @return 게시물을 24시간 이내에 읽었는지 여부
      */
-    public boolean postViewCheck(HttpServletRequest req, HttpServletResponse res, int id) {
-        Cookie oldCookie = null;
-        Cookie[] cookies = req.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("postView")) {
-                    oldCookie = cookie;
-                    break;
-                }
-            }
-        }
-
-        if (oldCookie != null) {
-            if (!oldCookie.getValue().contains("[" + Integer.toString(id) + "]")) {
-                oldCookie.setValue(oldCookie.getValue() + "_[" + id + "]");
-                oldCookie.setPath("/");
-                oldCookie.setMaxAge(60 * 60 * 24);
-                res.addCookie(oldCookie);
-
-                return false;
-            }
-            return true;
-        }
-        else {
-            Cookie newCookie = new Cookie("postView", "[" + id + "]");
-            newCookie.setPath("/");
-            newCookie.setMaxAge(60 * 60 * 24);
-            res.addCookie(newCookie);
-
+    public boolean postViewCheck(int memberId, int id) {
+        String str = (String) redisTemplate.opsForValue().get("POST " + id + " MEMBER " + memberId);
+        if (str == null) {
             return false;
         }
+        return true;
     }
 
     /**
