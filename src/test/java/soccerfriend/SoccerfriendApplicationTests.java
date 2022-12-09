c81ede07bcfe69ca553591f5e6847cca330fd22c
xeonu <sunwoo1029@naver.com>
2022-12-09 20:45:28 +0900

    test 수정

--- src/test/java/soccerfriend/SoccerfriendApplicationTests.java
+++ src/test/java/soccerfriend/SoccerfriendApplicationTests.java
@@ -1,13 +1,8 @@
 package soccerfriend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SoccerfriendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
