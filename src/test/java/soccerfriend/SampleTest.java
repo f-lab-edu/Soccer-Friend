b537e8898b8c6a06f35dfd3d95dff164591f849a
xeonu <sunwoo1029@naver.com>
2022-12-09 19:49:49 +0900

    gitignore 수정

---
+++ src/test/java/soccerfriend/SampleTest.java
@@ -0,0 +1,14 @@
+package soccerfriend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sample() {
+        int a = 3;
+        int b = 3;
+        Assertions.assertEquals(a, b);
+    }
+}
